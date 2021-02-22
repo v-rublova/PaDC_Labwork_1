c44911ad47472ae67c70cc6f776dc832f928dbd4
v.rublova <v.rublova@student.csn.khai.edu>
2021-02-22 16:22:31 +0200

    Task 1 (comparison of int and float arrays performance)

---
+++ src/com/calculations/Task1.java
@@ -0,0 +1,72 @@
+package com.calculations;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Task1 {
+
+    FileWriter fileWriter;
+    PrintWriter printWriter;
+
+    private void SetFileName(String name) throws IOException {
+        fileWriter = new FileWriter(name);
+        printWriter = new PrintWriter(fileWriter);
+    }
+
+    public void CloseFile() {
+        printWriter.close();
+    }
+
+    public void PerformCalc_int(String filename, int lim, boolean rc_sw) throws IOException {
+        this.SetFileName(filename);
+        for (int n = 0; n <= lim; n += 50) {
+            this.RowsCols(new int[n][n], n, rc_sw);
+        }
+        this.CloseFile();
+    }
+
+    public void PerformCalc_float(String filename, int lim, boolean rc_sw) throws IOException {
+        this.SetFileName(filename);
+        for (int n = 0; n <= lim; n += 50) {
+            this.RowsCols(new float[n][n], n, rc_sw);
+        }
+        this.CloseFile();
+    }
+    /**
+     * rc_sw stands for 'row-column switch'
+     * rw_sw = true - rows;
+     * rw_sw = false - columns.
+     */
+    private void RowsCols(int[][] array, int n, boolean rc_sw) {
+
+        long start = System.currentTimeMillis();
+        int bytes = n * n * 4 / 1024;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (rc_sw) array[i][j] = (i * j);
+                else array[j][i] = (i * j);
+            }
+        }
+        long end = System.currentTimeMillis();
+        printWriter.println(bytes + " \t" + (end - start));
+
+    }
+
+    private void RowsCols(float[][] array, int n, boolean rc_sw) {
+
+        long start = System.currentTimeMillis();
+        int bytes = n * n * 4 / 1024;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (rc_sw) array[i][j] = (i * j);
+                else array[j][i] = (i * j);
+            }
+        }
+        long end = System.currentTimeMillis();
+        printWriter.println(bytes + " \t" + (end - start));
+    }
+
+}
--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,8 +1,18 @@
 package com.main;
 
+import com.calculations.Task1;
+
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        Task1 obj = new Task1();
+        int lim = 12000;
+        obj.PerformCalc_int("log_int_rows", lim,true);
+        obj.PerformCalc_int("log_int_cols", lim,false);
+        obj.PerformCalc_float("log_float_rows", lim,true);
+        obj.PerformCalc_float("log_float_cols", lim,false);
     }
 }
